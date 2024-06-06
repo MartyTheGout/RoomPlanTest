af20712eff984b8d2eb629900ba1730b8fcea3a5
MartyTheGout <chihyung0@naver.com>
2024-06-06 19:03:50 +0900

    Initial Commit

---
+++ RoomPlanTest/RoomPlanTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomPlanTestApp.swift
+//  RoomPlanTest
+//
+//  Created by marty.academy on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RoomPlanTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
